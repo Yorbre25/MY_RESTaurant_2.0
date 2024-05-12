3b8c94b944affe8c732ee5539f55e738bdb76fa7
GeraldSalazar <geraldsalazar4@gmail.com>
2024-05-12 11:32:54 -0600

    updating .gitignore

--- frontend/my-rest-aurant/src/app/app.config.ts
+++ frontend/my-rest-aurant/src/app/app.config.ts
@@ -8,7 +8,11 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { envFirebase } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [ provideRouter(routes), provideHttpClient(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"my-rest-raurant-2","appId":"1:59314081117:web:fac699fd4cf982ca6f6cb4","databaseURL":"https://my-rest-raurant-2-default-rtdb.firebaseio.com","storageBucket":"my-rest-raurant-2.appspot.com","apiKey":"AIzaSyAsfx-2QWa8e1lX5Dv9TRf1WXdmsXNBAT0","authDomain":"my-rest-raurant-2.firebaseapp.com","messagingSenderId":"59314081117","measurementId":"G-ZXHJN212BP"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFunctions(() => getFunctions()))]
+  providers: [ provideRouter(routes), 
+    provideHttpClient(), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(envFirebase))), 
+    importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFunctions(() => getFunctions()))]
 };
